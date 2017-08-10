ba9e6d95fc6175776fe15b93f2dbe454e45b3ece
lpang36 <lawrencepang36@gmail.com>
2017-08-10 16:30:43 -0400

    deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ var fs = require('fs');
 var url = require('url');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/pw');
+
+var uristring = 
+process.env.MONGOLAB_URI ||
+process.env.MONGOHQ_URL ||
+'mongodb://localhost/pw';
+
+var port = process.env.PORT || 5000;
+
+mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
 
 var postSchema = mongoose.Schema({
 	title: String,

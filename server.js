2598816079fb89838e1c85d671d8634fb95019a7
lpang36 <lawrencepang36@gmail.com>
2017-08-10 16:48:59 -0400

    fix syntax error

--- server.js
+++ server.js
@@ -68,10 +68,10 @@ app.get('*', function(req,res){
 	res.render('notFound');
 });
 
-var server = app.listen(process.env.PORT || 5000), function () {
+var server = app.listen((process.env.PORT || 5000), function () {
 
    var host = server.address().address
    var port = server.address().port
 
    console.log("Example app listening at http://%s:%s", host, port)
-})
+});
